bd984b937a6de2e95e4a1bc5172c52ee0b9fe628
Runchao Han <me@runchao.rocks>
2024-07-19 14:26:24 +0800

    test

--- x/epoching/keeper/epochs_test.go
+++ x/epoching/keeper/epochs_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/babylonchain/babylon/testutil/datagen"
 	testhelper "github.com/babylonchain/babylon/testutil/helper"
+	"github.com/babylonchain/babylon/x/epoching/types"
 )
 
 func FuzzEpochs(f *testing.F) {
@@ -23,11 +24,10 @@ func FuzzEpochs(f *testing.F) {
 		require.Equal(t, epoch.EpochNumber, uint64(1))
 		require.Equal(t, epoch.FirstBlockHeight, uint64(1))
 
-		// set a random epoch interval
 		epochInterval := keeper.GetParams(ctx).EpochInterval
 
 		// increment a random number of new blocks
-		numIncBlocks := r.Uint64()%1000 + 1
+		numIncBlocks := r.Uint64()%100 + 1
 		var err error
 		for i := uint64(0); i < numIncBlocks-1; i++ {
 			// TODO: Figure out why when ctx height is 1, ApplyEmptyBlockWithVoteExtension
@@ -45,5 +45,57 @@ func FuzzEpochs(f *testing.F) {
 		require.Equal(t, expectedEpochNumber, actualNewEpoch.EpochNumber)
 		require.Equal(t, epochInterval, actualNewEpoch.CurrentEpochInterval)
 		require.Equal(t, (expectedEpochNumber-1)*epochInterval+1, actualNewEpoch.FirstBlockHeight)
+	})
+}
+
+func FuzzEpochs_UpdateEpochInterval(f *testing.F) {
+	datagen.AddRandomSeedsToFuzzer(f, 10)
+
+	f.Fuzz(func(t *testing.T, seed int64) {
+		r := rand.New(rand.NewSource(seed))
+
+		h := testhelper.NewHelper(t)
+		keeper := h.App.EpochingKeeper
+
+		// increment a random number of new blocks
+		numIncBlocks := r.Uint64()%100 + 1
+		var err error
+		for i := uint64(0); i < numIncBlocks-1; i++ {
+			// When ctx height is 1, ApplyEmptyBlockWithVoteExtension
+			// will still give ctx height 1 once, then start to increment
+			_, err = h.ApplyEmptyBlockWithVoteExtension(r)
+			require.NoError(t, err)
+		}
+		// get current epoch metadata
+		epoch := keeper.GetEpoch(h.Ctx)
+
+		// update the epoch interval in params
+		newEpochInterval := datagen.RandomInt(r, 20) + 2
+		newParams := types.Params{EpochInterval: newEpochInterval}
+		err = keeper.SetParams(h.Ctx, newParams)
+		require.NoError(t, err)
+
+		// ensure the current epoch metadata is not affected
+		epoch2 := keeper.GetEpoch(h.Ctx)
+		require.Equal(t, epoch, epoch2)
+
+		// enter the last block of the current epoch
+		lastHeightOfEpoch := epoch.GetLastBlockHeight()
+		for uint64(h.Ctx.HeaderInfo().Height) < lastHeightOfEpoch {
+			_, err = h.ApplyEmptyBlockWithVoteExtension(r)
+			require.NoError(t, err)
+		}
+		// enter the next block and thus 1st block of the next epoch
+		_, err = h.ApplyEmptyBlockWithVoteExtension(r)
+		require.NoError(t, err)
+		// ensure
+		// - the epoch has incremented
+		// - epoch interval is updated
+		// - first/last height of the epoch is correct
+		newEpoch := keeper.GetEpoch(h.Ctx)
+		require.Equal(t, epoch.EpochNumber+1, newEpoch.EpochNumber)
+		require.Equal(t, newEpochInterval, newEpoch.CurrentEpochInterval)
+		require.Equal(t, uint64(h.Ctx.HeaderInfo().Height), newEpoch.FirstBlockHeight)
+		require.Equal(t, uint64(h.Ctx.HeaderInfo().Height)+newEpochInterval-1, newEpoch.GetLastBlockHeight())
 	})
 }
